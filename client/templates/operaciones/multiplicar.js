fac36fc4bd3743e7a2b78fc935470a9ea12084e3
edgar95 <ecf_06@hotmail.com>
2016-04-11 20:08:21 +0200

    corrección de los simbolos de las operaciones

--- client/templates/operaciones/multiplicar.js
+++ client/templates/operaciones/multiplicar.js
@@ -5,6 +5,7 @@ Template.multiplicar.events({
 
         var multiplica = {
             operador1: evento.target.op1.value,
+            operacion: "*",
             operador2: evento.target.op2.value,
             igual: "=",
             resultado: parseInt(evento.target.op1.value) * parseInt(evento.target.op2.value)
--- client/templates/operaciones/restar.js
+++ client/templates/operaciones/restar.js
@@ -5,6 +5,7 @@ Template.restar.events({
 
         var resta = {
             operador1: evento.target.op1.value,
+            operacion: "-",
             operador2: evento.target.op2.value,
             igual: "=",
             resultado: parseInt(evento.target.op1.value) - parseInt(evento.target.op2.value)
